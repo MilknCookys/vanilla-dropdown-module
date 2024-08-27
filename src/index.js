9e71fc16f69cb118b1cebe39b7f75301dc1cdd24
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-08-27 10:41:30 +1000

    Basic dropdown template done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './styles.css';
 
-console.log('Hello World!');
+export default function dropdownComponent(targetElement, container) {
+  const dropdownTarget = document.querySelectorAll(targetElement);
+
+  dropdownTarget.forEach((target) => {
+    target.addEventListener('click', () => {
+      const dropdownContainer = target.querySelector(container);
+      dropdownContainer.classList.toggle('hidden');
+    });
+  });
+}
+
+dropdownComponent();
